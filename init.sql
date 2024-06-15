e5352d1ccca3a18efd8b4ae2f630be5be05baae6
Fabio Marcos <fabiojava@hotmail.com>
2024-06-15 06:51:13 -0700

    my data

---
+++ init.sql
@@ -0,0 +1,10 @@
+
+
+CREATE EXTENSION IF NOT EXISTS vector;
+
+CREATE TABLE IF NOT EXISTS embeddings (
+  id SERIAL PRIMARY KEY,
+  embedding vector,
+  text text,
+  created_at timestamptz DEFAULT now()
+);
